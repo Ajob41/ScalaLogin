5530b1920722130945c74c031b67f4b85fd1d2ae
Ajob41 <jobwrsme41@gmail.com>
2025-04-11 09:51:46 -0500

    app.scala changed

--- HttpOfThings/HttpOfthings.scala
+++ HttpOfThings/HttpOfthings.scala
@@ -1,26 +1,28 @@
-
 import com.sun.net.httpserver.{HttpServer, HttpHandler, HttpExchange}
-def isHttpMethodOf(httpMethod:String,request:HttpExchange):Boolean = {
-    val whichHttpMethod = if(request.getRequestMethod().equals(httpMethod.toUpperCase())) {
-       return isPostMethod(httpMethod,request)
-    }
-    false;
+def isHttpMethodOf(httpMethod: String, request: HttpExchange): Unit = {
+  httpMethod.toUpperCase match {
+    case "POST" => isPostMethod(request)
+    case "GET"  => isGetMethod(request)
+  }
+
 }
 
-def isPostMethod(httpMethod:String,request:HttpExchange):Boolean = {
-    val httpMethodOf = "POST";
-    return request.getRequestMethod().equals(httpMethodOf)
+def isPostMethod(request: HttpExchange): Boolean = {
+  return request.getRequestMethod.equalsIgnoreCase("POST")
 }
 
-def isGetMethod(httpMethod:String,request:HttpExchange):Boolean = {
-    val httpMethodOf = "GET";
-    return request.getRequestMethod().equals(httpMethodOf)
+def isGetMethod(request: HttpExchange): Boolean = {
+  return request.getRequestMethod.equalsIgnoreCase("GET")
 }
 
-def replyBack(data:String):String = {
+
+
+
+
+def replyBack(data: String): String = {
   "";
 }
 
-def getData(data:String):String = {
+def getData(data: String): String = {
   return data;
 }
